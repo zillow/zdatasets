d4659b498f124c2c08ec8c29d248c7e6429bfbb9
Peilun Li <peilunl@zillowgroup.com>
2022-09-16 12:04:09 -0700

    More restricted secret return type; secret cache; add tests

---
+++ datasets/tests/utils/test_secret.py
@@ -0,0 +1,115 @@
+from cmath import log
+from datasets.utils.secret import Secret
+import pytest
+import unittest
+from unittest import mock
+from moto import mock_secretsmanager
+import os
+import boto3
+import base64
+
+case = unittest.TestCase()
+
+def test_secret_value_formatter():
+    assert Secret._secret_value_formatter({"example": 1}) == {"example": "1"}
+    assert Secret._secret_value_formatter("1") == "1"
+    assert Secret._secret_value_formatter(1) == "1"
+
+def test_fetch_raw_secret():
+    assert Secret(raw_secret={"key":"value"}).value == {"key":"value"}
+    assert Secret(raw_secret={"key":"value"}, key="key").value == "value"
+    with pytest.raises(KeyError):
+        Secret(raw_secret={"key":"value"}, key="wrong_key").value
+
+@mock.patch.dict(os.environ, {f"EXAMPLE_SECRET": '{"key": "value"}'})
+def test_fetch_env_secret_json_decodable_dict():
+    assert Secret(env_var="EXAMPLE_SECRET").value == {"key": "value"}
+    assert Secret(env_var="EXAMPLE_SECRET", key="key").value == "value"
+    with pytest.raises(KeyError):
+        Secret(env_var="EXAMPLE_SECRET", key="wrong_key").value
+
+@mock.patch.dict(os.environ, {f"EXAMPLE_SECRET": '"example_value"'})
+def test_fetch_env_secret_json_decodable_str():
+    assert Secret(env_var="EXAMPLE_SECRET").value == "example_value"
+    with pytest.raises(TypeError):
+        Secret(env_var="EXAMPLE_SECRET", key="key").value
+
+
+@mock.patch.dict(os.environ, {f"EXAMPLE_SECRET": "example_value"})
+def test_fetch_env_secret_not_json_decodable():
+    assert Secret(env_var="EXAMPLE_SECRET").value == "example_value"
+    with pytest.raises(ValueError):
+        Secret(env_var="EXAMPLE_SECRET", key="key").value
+
+@mock_secretsmanager
+def test_fetch_aws_secret():
+    from datasets.utils.secret import secret_cache, logger
+    conn = boto3.client("secretsmanager", region_name="us-west-2")
+    conn.create_secret(Name="json-decodable-dict", SecretString='{"key": "value"}')
+    conn.create_secret(Name="json-decodable-str", SecretString='"example_value"')
+    conn.create_secret(Name="not-json-decodable", SecretString="example_value")
+    
+    # Json decodable dict
+    assert Secret(aws_secret_arn="json-decodable-dict").value == {"key": "value"}
+    # Test secret is cached
+    assert secret_cache["aws_secret"]["json-decodable-dict"] == '{"key": "value"}'
+    # Test secret fetched from cache
+    conn.update_secret(SecretId="json-decodable-dict", SecretString='{"key": "new_value"}')
+    with case.assertLogs(logger=logger,level="INFO") as cm:
+        assert Secret(aws_secret_arn="json-decodable-dict", key="key").value == "value"
+    assert any("Using secret from cache json-decodable-dict" in log for log in cm.output)
+    # Test force_reload
+    assert Secret(aws_secret_arn="json-decodable-dict", 
+        key="key", force_reload=True).value == "new_value"
+    # Test cache updated after force_reload
+    assert secret_cache["aws_secret"]["json-decodable-dict"] == '{"key": "new_value"}'
+
+    with pytest.raises(KeyError):
+        Secret(aws_secret_arn="json-decodable-dict", key="wrong_key").value
+    
+    # Json decodable str
+    assert Secret(aws_secret_arn="json-decodable-str").value == "example_value"
+    with pytest.raises(TypeError):
+        Secret(aws_secret_arn="json-decodable-str", key="key").value
+    
+    # Not json decodable str
+    assert Secret(aws_secret_arn="not-json-decodable").value == "example_value"
+    with pytest.raises(ValueError):
+        Secret(aws_secret_arn="not-json-decodable", key="key").value
+
+@mock.patch("datasets.utils.secret.get_current_namespace")
+@mock.patch("datasets.utils.secret.config")
+@mock.patch("datasets.utils.secret.client")
+def test_fetch_cluster_secret(client, config, namespace):
+    from datasets.utils.secret import secret_cache, logger
+    example_cluster_secret = {
+        "key": base64.b64encode(b"value"),
+    }
+
+    example_new_cluster_secret = {
+        "key": base64.b64encode(b"new_value"),
+    }
+
+    client.CoreV1Api.return_value.read_namespaced_secret.return_value.data = example_cluster_secret
+    assert Secret(cluster_secret_name="test").value == {"key": "value"}
+    # Test secret is cached
+    assert secret_cache["cluster_secret"]["test"] == {"key": "value"}
+    # Test secret fetched from cache
+    client.CoreV1Api.return_value.read_namespaced_secret.return_value.data = example_new_cluster_secret
+    with case.assertLogs(logger=logger,level="INFO") as cm:
+        assert Secret(cluster_secret_name="test", key="key").value == "value"
+    assert any("Using secret from cache test" in log for log in cm.output)
+    # Test force_reload
+    assert Secret(cluster_secret_name="test", key="key", force_reload=True).value == "new_value"
+    # Test cache updated after force_reload
+    assert secret_cache["cluster_secret"]["test"] == {"key": "new_value"}
+
+    with pytest.raises(KeyError):
+        Secret(cluster_secret_name="test", key="wrong_key").value
+
+def test_variable_validation():
+    with pytest.raises(ValueError):
+        Secret(env_var="TEST_ENV_VAR", raw_secret={"key": "value"}).value
+    
+    with pytest.raises(ValueError):
+        Secret(raw_secret={"key": "value"}, key=1).value
--- datasets/utils/secret.py
+++ datasets/utils/secret.py
@@ -2,11 +2,11 @@ import base64
 import json
 import logging
 import os
+from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Dict, Optional, Union
+from typing import Dict, Union
 
 import boto3
-from botocore.exceptions import ClientError
 from kubernetes import client, config
 from tenacity import (
     Retrying,
@@ -18,7 +18,13 @@ from tenacity import (
 
 logger = logging.getLogger(__name__)
 
-secret_return_type = Optional[Union[str, Dict[str, Any]]]
+# Ensure we return a non-None value
+# Prefer to throw exception rather than returning None if secret can't be found
+secret_return_type = Union[str, Dict[str, str]]
+
+# Global secret cache to prevent redundant
+# secret retrieval calls to external system if possible
+secret_cache = defaultdict(dict)
 
 
 @dataclass
@@ -36,19 +42,24 @@ class Secret:
         raw_secret: raw secret value
             secret value to be pulled directly through the variable
 
-    An optional key variable in case if secret source contains multiple secrets as a dict
-        return secret[key] if provided, otherwise return full secret
+    Optional variables:
+        key: if secret source contains multiple secrets as a dict,
+             return secret[key] if provided, otherwise return full secret
+        force_reload: ignore cached secret (if exists) and force reload from    source
 
     Interface requirement:
-        A value property to return the actual secret
+        A "value" property to return the actual secret value,
+        either as a str (for single secret value) or a Dict[str, str] (for a group of secrets)
     """
 
     cluster_secret_name: str = None
     aws_secret_arn: str = None
     env_var: str = None
-    raw_secret: Union[str, Dict[str, str]] = None
+    raw_secret: secret_return_type = None
 
     key: str = None
+
+    force_reload: bool = False
 
     @property
     def value(self) -> secret_return_type:
@@ -70,7 +81,8 @@ class Secret:
 
     def _variable_validation(self):
         """
-        Ensure exact one secret source variable provided. Throw an Error otherwise.
+        Ensure exact one secret source variable provided.
+        Ensure key is of str type if provided
         """
         secret_source_variables = ["cluster_secret_name", "aws_secret_arn", "env_var", "raw_secret"]
 
@@ -81,19 +93,42 @@ class Secret:
         if secret_source_variable_used.count(True) != 1:
             raise ValueError("Must provide exact one secret source variable!")
 
+        if self.key is not None and not isinstance(self.key, str):
+            raise ValueError("key should be an string!")
+
     def _fetch_cluster_secret(self) -> secret_return_type:
-        config.load_incluster_config()
-        core_api = client.CoreV1Api()
-        namespace = get_current_namespace()
-        for attempt in Retrying(stop=stop_after_attempt(3), wait=wait_fixed(5), reraise=True):
-            with attempt:
-                secret_value = core_api.read_namespaced_secret(self.cluster_secret_name, namespace).data
-        for k, v in secret_value.items():
-            secret_value[k] = base64.b64decode(v)
-        return secret_value.get(self.key) if self.key is not None else secret_value
+        # Try to fetch from cache first
+        global secret_cache
+        secret_from_cache = secret_cache["cluster_secret"].get(self.cluster_secret_name)
+        if secret_from_cache is not None and not self.force_reload:
+            logger.info(f"Using secret from cache {self.cluster_secret_name}")
+            secret_value = secret_from_cache
+        else:
+            config.load_incluster_config()
+            core_api = client.CoreV1Api()
+            namespace = get_current_namespace()
+            for attempt in Retrying(stop=stop_after_attempt(3), wait=wait_fixed(5), reraise=True):
+                with attempt:
+                    raw_secret_value = core_api.read_namespaced_secret(
+                        self.cluster_secret_name, namespace
+                    ).data
+
+            # Ensure we convert to Dict[str, str] type
+            secret_value = {}
+            for k, v in raw_secret_value.items():
+                # The last decode("utf-8") converts bytes type to str
+                secret_value[str(k)] = base64.b64decode(v).decode("utf-8")
+            secret_cache["cluster_secret"][self.cluster_secret_name] = secret_value
+
+        return secret_value[self.key] if self.key is not None else secret_value
 
     def _fetch_aws_secret(self) -> secret_return_type:
-        try:
+        global secret_cache
+        secret_from_cache = secret_cache["aws_secret"].get(self.aws_secret_arn)
+        if secret_from_cache is not None and not self.force_reload:
+            logger.info(f"Using secret from cache {self.aws_secret_arn}")
+            secret_value = secret_from_cache
+        else:
             # We have AWS_REGION env var injected in most of our workloads
             secrets_manager_client = boto3.Session(region_name=os.getenv("AWS_REGION", "us-west-2")).client(
                 "secretsmanager"
@@ -111,33 +146,51 @@ class Secret:
                     get_secret_value_response = secrets_manager_client.get_secret_value(
                         SecretId=self.aws_secret_arn
                     )
-        except ClientError as e:
-            error_code = e.response["Error"]["Code"]
-            if error_code == "DecryptionFailureException":
-                logger.exception(
-                    "Secrets Manager can't decrypt the protected secret text using the provided KMS key."
-                )
-            elif error_code == "ResourceNotFoundException":
-                logger.exception("Cannot find the resource asked for.")
-            raise e
 
-        secret_value = get_secret_value_response.get("SecretString")
+            secret_value = get_secret_value_response.get("SecretString")
+            if not secret_value:
+                raise ValueError("Empty SecretString response from boto3 calls!")
+
+            secret_cache["aws_secret"][self.aws_secret_arn] = secret_value
+
         return self._try_decode_with_json(secret_value)
 
     def _fetch_env_secret(self) -> secret_return_type:
         env_value = os.getenv(self.env_var)
+        if env_value is None:
+            raise ValueError(f"Env var {self.env_var} does not exist!")
         return self._try_decode_with_json(env_value)
 
     def _fetch_raw_secret(self) -> secret_return_type:
-        return self.raw_secret.get(self.key) if self.key is not None else self.raw_secret
+        self.raw_secret = self._secret_value_formatter(self.raw_secret)
+        return self.raw_secret[self.key] if self.key is not None else self.raw_secret
 
-    def _try_decode_with_json(self, secret_value) -> secret_return_type:
+    def _try_decode_with_json(self, secret_value: str) -> secret_return_type:
+        if not isinstance(secret_value, str):
+            raise ValueError(f"input secret_value should be an string! Getting {type(secret_value)} instead!")
+
         try:
             decoded_secret_value = json.loads(secret_value)
-            return decoded_secret_value.get(self.key) if self.key is not None else decoded_secret_value
+            decoded_secret_value = self._secret_value_formatter(decoded_secret_value)
+            return decoded_secret_value[self.key] if self.key is not None else decoded_secret_value
         except json.decoder.JSONDecodeError:
-            # env var value is not json compatible, return raw string instead
+            # if value is not json compatible, return raw string if key is not provided
+            if self.key is not None:
+                raise ValueError(f"Secret value is not json decodable! Can not fetch key {self.key}")
             return secret_value
+
+    @staticmethod
+    def _secret_value_formatter(secret_value) -> secret_return_type:
+        """
+        Ensure secret is of type Union[str, Dict[str, str]]
+        """
+        if isinstance(secret_value, dict):
+            converted_secret_value = {}
+            for k, v in secret_value.items():
+                converted_secret_value[str(k)] = str(v)
+            return converted_secret_value
+        else:
+            return str(secret_value)
 
 
 def get_current_namespace() -> str:
